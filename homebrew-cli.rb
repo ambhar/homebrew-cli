abd1cd5a33382d542902ccc74fba4cc871c8714d
ambhar <amol.freelancing@gmail.com>
2022-07-13 17:01:50 +0530

    updated_file

--- homebrew-cli.rb
+++ homebrew-cli.rb
@@ -4,8 +4,7 @@ class HomebrewCli < Formula
   url "https://github.com/ambhar/homebrew-cli/archive/refs/tags/v0.0.2.tar.gz"
   sha256 "9dcddd84d137a9d9855999b6c9c866f2f05cc2a3b211b41be86bf2aab2c3bb3f"
   license "MIT"
-
   def install
-    bin.install "https://github.com/ambhar/homebrew-cli/add_cli.py"
+    bin.install "add_cli.py"
   end
 end
