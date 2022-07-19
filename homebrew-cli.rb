29fd45e8d890558e2e9887228bdb9de1849c93df
ambhar <amol.freelancing@gmail.com>
2022-07-19 15:18:19 +0530

    config file brew

--- homebrew-cli.rb
+++ homebrew-cli.rb
@@ -1,10 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class HomebrewCli < Formula
-  desc "Homebrew add cli"
-  homepage "https://github.com/ambhar/homebrew-cli"
-  url "https://github.com/ambhar/homebrew-cli/archive/refs/tags/v0.0.2.tar.gz"
-  sha256 "9dcddd84d137a9d9855999b6c9c866f2f05cc2a3b211b41be86bf2aab2c3bb3f"
-  license "MIT"
+  desc ""
+  homepage ""
+  url "https://github.com/ambhar/homebrew-cli/archive/refs/tags/v0.1.tar.gz"
+  sha256 "3cc933b7aab49659efce291ed878aba9865d61579be2321da5b844cefe844b72"
+  license ""
+
+  # depends_on "cmake" => :build
+
   def install
-    bin.install "add_cli"
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test homebrew-cli`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
